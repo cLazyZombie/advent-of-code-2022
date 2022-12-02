c691eb27f5171835cff0faf645e1a5768a1947ec
cLazyZombie <cLazyZombie@gmail.com>
2022-12-02 16:10:29 +0900

    day 02 part 2

--- src/day_02.rs
+++ src/day_02.rs
@@ -36,13 +36,45 @@ impl RPS {
             _ => 0,
         }
     }
+}
 
-    pub fn score(&self, other: RPS) -> u32 {
-        self.shape_score() + self.beats_score(other)
+fn score(mine: RPS, other: RPS) -> u32 {
+    mine.shape_score() + mine.beats_score(other)
+}
+
+#[derive(Debug, Copy, Clone)]
+enum MatchResult {
+    Win,
+    Lose,
+    Draw,
+}
+
+impl<'a> From<&'a str> for MatchResult {
+    fn from(s: &'a str) -> Self {
+        match s {
+            "X" => MatchResult::Lose,
+            "Y" => MatchResult::Draw,
+            "Z" => MatchResult::Win,
+            _ => panic!("Invalid input"),
+        }
     }
 }
 
-pub fn total_score_by_guide(input: &str) -> u32 {
+fn expected_rps(other: RPS, result: MatchResult) -> RPS {
+    match (other, result) {
+        (RPS::Rock, MatchResult::Win) => RPS::Paper,
+        (RPS::Rock, MatchResult::Draw) => RPS::Rock,
+        (RPS::Rock, MatchResult::Lose) => RPS::Scissors,
+        (RPS::Paper, MatchResult::Win) => RPS::Scissors,
+        (RPS::Paper, MatchResult::Draw) => RPS::Paper,
+        (RPS::Paper, MatchResult::Lose) => RPS::Rock,
+        (RPS::Scissors, MatchResult::Win) => RPS::Rock,
+        (RPS::Scissors, MatchResult::Draw) => RPS::Scissors,
+        (RPS::Scissors, MatchResult::Lose) => RPS::Paper,
+    }
+}
+
+pub fn total_score_part1(input: &str) -> u32 {
     let mut total = 0;
     for line in input.lines() {
         let mut it = line.trim().split_ascii_whitespace();
@@ -51,8 +83,25 @@ pub fn total_score_by_guide(input: &str) -> u32 {
         }
 
         let opponent: RPS = it.next().unwrap().into();
-        let guide: RPS = it.next().unwrap().into();
-        let score = guide.score(opponent);
+        let mine: RPS = it.next().unwrap().into();
+        let score = score(mine, opponent);
+        total += score
+    }
+    total
+}
+
+pub fn total_score_part2(input: &str) -> u32 {
+    let mut total = 0;
+    for line in input.lines() {
+        let mut it = line.trim().split_ascii_whitespace();
+        if it.clone().count() != 2 {
+            continue;
+        }
+
+        let opponent: RPS = it.next().unwrap().into();
+        let guide: MatchResult = it.next().unwrap().into();
+        let mine = expected_rps(opponent, guide);
+        let score = score(mine, opponent);
         total += score
     }
     total
@@ -69,14 +118,32 @@ mod tests {
         B X
         C Z"#;
 
-        let score = total_score_by_guide(input);
+        let score = total_score_part1(input);
         assert_eq!(score, 15);
     }
 
     #[test]
     fn part1() {
         let input = include_str!("../input/day_02.txt");
-        let score = total_score_by_guide(input);
+        let score = total_score_part1(input);
         assert_eq!(score, 13484);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r#"
+        A Y
+        B X
+        C Z"#;
+
+        let score = total_score_part2(input);
+        assert_eq!(score, 12);
+    }
+
+    #[test]
+    fn part2() {
+        let input = include_str!("../input/day_02.txt");
+        let score = total_score_part2(input);
+        assert_eq!(score, 13433);
     }
 }
