40f9c8fbb2baca1bc4d7cfcaaadd2fb1a0cd16c8
cLazyZombie <cLazyZombie@gmail.com>
2022-12-07 16:56:43 +0900

    day 07 part 2

--- src/day_07.rs
+++ src/day_07.rs
@@ -5,11 +5,36 @@ pub fn solve_part1(input: &str) -> u32 {
     lines.next(); // skip dir
 
     let mut sum_atmost = 0;
-    let _sum = get_directory_size(&mut lines, &mut sum_atmost);
+    let _sum = get_directory_size(&mut lines, &mut sum_atmost, &mut 0, 0);
     sum_atmost
 }
 
-fn get_directory_size<'a>(lines: &'a mut Lines, sum_atmost: &mut u32) -> u32 {
+pub fn solve_part2(input: &str) -> u32 {
+    let mut lines = input.lines();
+    lines.next(); // skip dir
+
+    // pass 1 for sum total space
+    let mut _sum_atmost = 0;
+    let sum = get_directory_size(&mut lines, &mut _sum_atmost, &mut 0, 0);
+    let cur_space = 70000000 - sum;
+    let need_space = 30000000 - cur_space;
+
+    // pass 2 for smallest space that can fit needed space
+    let mut lines = input.lines();
+    lines.next(); // skip dir
+    let mut smallest = std::u32::MAX;
+
+    let _sum = get_directory_size(&mut lines, &mut _sum_atmost, &mut smallest, need_space);
+
+    smallest
+}
+
+fn get_directory_size<'a>(
+    lines: &'a mut Lines,
+    sum_atmost: &mut u32,
+    smallest: &mut u32,
+    need_space: u32,
+) -> u32 {
     let mut sum = 0;
     while let Some(line) = lines.next() {
         let (cmd1, cmd2, cmd3) = parse_line(&line);
@@ -19,7 +44,7 @@ fn get_directory_size<'a>(lines: &'a mut Lines, sum_atmost: &mut u32) -> u32 {
                 break;
             }
             ("$", "cd", Some(_dir)) => {
-                let sub_sum = get_directory_size(lines, sum_atmost);
+                let sub_sum = get_directory_size(lines, sum_atmost, smallest, need_space);
                 sum += sub_sum;
             }
             ("dir", _dir, _) => {
@@ -37,6 +62,10 @@ fn get_directory_size<'a>(lines: &'a mut Lines, sum_atmost: &mut u32) -> u32 {
 
     if sum <= 100_000 {
         *sum_atmost += sum;
+    }
+
+    if sum >= need_space && sum < *smallest {
+        *smallest = sum;
     }
 
     sum
@@ -113,5 +142,40 @@ $ cd .."#;
         let input = include_str!("../input/day_07.txt");
         let answer = solve_part1(input);
         assert_eq!(answer, 1743217);
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        let input = r#"$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"#;
+        let answer = solve_part2(input);
+        assert_eq!(answer, 24933642);
+    }
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../input/day_07.txt");
+        let answer = solve_part2(input);
+        assert_eq!(answer, 8319096);
     }
 }
