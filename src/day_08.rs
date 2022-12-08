fe9297c3ba9583263fed6bb7bc39fcf15d49ab4f
cLazyZombie <cLazyZombie@gmail.com>
2022-12-08 14:45:52 +0900

    day 08 part 2

--- src/day_08.rs
+++ src/day_08.rs
@@ -13,6 +13,24 @@ pub fn solve_part1(input: &str) -> u32 {
     sum
 }
 
+pub fn solve_part2(input: &str) -> u32 {
+    let grid = load_grid(input);
+
+    dbg!(get_score(&grid, 2, 3));
+
+    let mut highest = 0;
+    for y in 0..grid.len() {
+        for x in 0..grid[y].len() {
+            let score = get_score(&grid, x, y);
+            if score > highest {
+                highest = score;
+            }
+        }
+    }
+
+    highest
+}
+
 fn load_grid(input: &str) -> Vec<Vec<u8>> {
     let mut grid = Vec::new();
     for line in input.lines() {
@@ -85,6 +103,60 @@ fn is_visible(grid: &Vec<Vec<u8>>, x: usize, y: usize) -> bool {
     false
 }
 
+fn get_score(grid: &Vec<Vec<u8>>, x: usize, y: usize) -> u32 {
+    let cur = grid[y][x];
+
+    // check left
+    let mut left_score = 0;
+    for i in (0..x).rev() {
+        left_score += 1;
+
+        let h = grid[y][i];
+        if h >= cur {
+            break;
+        }
+    }
+
+    // check right
+    let mut right_score = 0;
+    for i in x + 1..grid[0].len() {
+        right_score += 1;
+
+        let h = grid[y][i];
+        if h >= cur {
+            break;
+        }
+    }
+
+    // check up
+    let mut up_score = 0;
+
+    for i in (0..y).rev() {
+        up_score += 1;
+
+        let h = grid[i][x];
+        if h >= cur {
+            break;
+        }
+    }
+
+    // check down
+    let mut down_score = 0;
+
+    for i in y + 1..grid.len() {
+        down_score += 1;
+
+        let h = grid[i][x];
+        if h >= cur {
+            break;
+        }
+    }
+
+    let score = left_score * right_score * up_score * down_score;
+    println!("{} {} = {}", x, y, score);
+    score
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -97,13 +169,31 @@ mod tests {
         33549
         35390"#;
         let answer = solve_part1(input);
-        assert_eq!(answer, 1681);
+        assert_eq!(answer, 21);
     }
 
     #[test]
     fn test_part1() {
         let input = include_str!("../input/day_08.txt");
         let answer = solve_part1(input);
-        assert_eq!(answer, 256);
+        assert_eq!(answer, 1681);
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        let input = r#"30373
+        25512
+        65332
+        33549
+        35390"#;
+        let answer = solve_part2(input);
+        assert_eq!(answer, 8);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../input/day_08.txt");
+        let answer = solve_part2(input);
+        assert_eq!(answer, 201684);
     }
 }
