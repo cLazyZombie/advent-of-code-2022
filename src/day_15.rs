972fb889079db3b21fb3f82ab84ac0a02e82cdbd
cLazyZombie <cLazyZombie@gmail.com>
2022-12-16 11:40:04 +0900

    day 15 part2

--- src/day_15.rs
+++ src/day_15.rs
@@ -32,42 +32,60 @@ pub fn solve_part1(input: &str, row: i32) -> i32 {
 
 pub fn solve_part2(input: &str, size: i32) -> i64 {
     let parsed = parse_input(input);
-    let mut init = ConvexHull::new();
-    init.points = vec![(0, 0), (size, 0), (size, size), (0, size)];
+    find_empty_pos(&parsed, size)
+}
 
-    let mut world = vec![init];
-
-    for (sx, sy, bx, by) in parsed {
-        let len = (bx - sx).abs() + (by - sy).abs();
-
-        let planes = [
-            Plane::from_point(Dir::UpLeft, (sx - len, sy)),
-            Plane::from_point(Dir::UpRight, (sx + len, sy)),
-            Plane::from_point(Dir::DownRight, (sx + len, sy)),
-            Plane::from_point(Dir::DownLeft, (sx - len, sy)),
-        ];
-
-        // culling
-        let mut remaining = Vec::new();
-        for plane in &planes {
-            let mut candidates = Vec::new();
-
-            for convex in &mut world {
-                let (front, back) = convex.divide(plane);
-                if let Some(front) = front {
-                    remaining.push(front);
-                }
-
-                if let Some(back) = back {
-                    candidates.push(back);
-                }
+fn find_empty_pos(input: &[(i32, i32, i32, i32)], size: i32) -> i64 {
+    for &(sx, sy, bx, by) in input {
+        let points = get_cover_points(sx, sy, bx, by);
+        for (x, y) in points {
+            if x < 0 || x > size || y < 0 || y > size {
+                continue;
             }
-            world = candidates;
+
+            if is_empty_at_pos(input, x, y) {
+                return (x as i64) * 4000000 + (y as i64);
+            }
         }
-        world = remaining;
     }
-    println!("world: {:?}", world);
     0
+}
+
+fn is_empty_at_pos(input: &[(i32, i32, i32, i32)], x: i32, y: i32) -> bool {
+    for &(sx, sy, bx, by) in input {
+        if is_in_area(sx, sy, bx, by, x, y) {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn is_in_area(sx: i32, sy: i32, bx: i32, by: i32, px: i32, py: i32) -> bool {
+    let len = (bx - sx).abs() + (by - sy).abs();
+    let dist = (px - sx).abs() + (py - sy).abs();
+
+    dist <= len
+}
+
+fn get_cover_points(sx: i32, sy: i32, bx: i32, by: i32) -> Vec<(i32, i32)> {
+    let mut points = Vec::new();
+
+    let len = (bx - sx).abs() + (by - sy).abs() + 1;
+
+    for i in -len..=len {
+        let x = sx + i;
+        let y1 = sy + (len - i.abs());
+        let y2 = sy - (len - i.abs());
+        if y1 == y2 {
+            points.push((x, y1));
+        } else {
+            points.push((x, y1));
+            points.push((x, y2));
+        }
+    }
+
+    points
 }
 
 fn impossible_beacon_count_at_row(input: &[(i32, i32, i32, i32)], row: i32) -> i32 {
@@ -110,140 +128,6 @@ fn impossible_beacon_count_at_row(input: &[(i32, i32, i32, i32)], row: i32) -> i
     (impossibles.len() - prev_count) as i32
 }
 
-#[derive(Debug, PartialEq, Eq, Clone)]
-struct ConvexHull {
-    points: Vec<(i32, i32)>,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-enum Dir {
-    Up,
-    Down,
-    Left,
-    Right,
-    UpRight,
-    DownRight,
-    DownLeft,
-    UpLeft,
-}
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-struct Plane {
-    normal: Dir,
-    distance: i32,
-}
-
-impl Plane {
-    fn new(normal: Dir, distance: i32) -> Plane {
-        Plane { normal, distance }
-    }
-
-    fn from_point(normal: Dir, p: (i32, i32)) -> Plane {
-        let distance = match normal {
-            Dir::Up => -p.1,
-            Dir::Down => p.1,
-            Dir::Left => -p.0,
-            Dir::Right => p.0,
-            Dir::UpRight => -p.0 + p.1,
-            Dir::DownRight => p.0 + p.1,
-            Dir::DownLeft => p.0 - p.1,
-            Dir::UpLeft => -p.0 - p.1,
-        } + 1;
-
-        Plane { normal, distance }
-    }
-
-    fn distance(&self, p: (i32, i32)) -> i32 {
-        match self.normal {
-            Dir::Up => -p.1 - self.distance,
-            Dir::Down => p.1 - self.distance,
-            Dir::Left => -p.0 - self.distance,
-            Dir::Right => p.0 - self.distance,
-            Dir::UpRight => p.0 - p.1 - self.distance,
-            Dir::DownRight => p.0 + p.1 - self.distance,
-            Dir::DownLeft => -p.0 + p.1 - self.distance,
-            Dir::UpLeft => -p.0 - p.1 - self.distance,
-        }
-    }
-
-    fn opposite(&self) -> Plane {
-        Plane {
-            normal: match self.normal {
-                Dir::Up => Dir::Down,
-                Dir::Down => Dir::Up,
-                Dir::Left => Dir::Right,
-                Dir::Right => Dir::Left,
-                Dir::UpRight => Dir::DownLeft,
-                Dir::DownRight => Dir::UpLeft,
-                Dir::DownLeft => Dir::UpRight,
-                Dir::UpLeft => Dir::DownRight,
-            },
-            distance: -self.distance + 1,
-        }
-    }
-}
-
-impl ConvexHull {
-    fn new() -> ConvexHull {
-        ConvexHull { points: Vec::new() }
-    }
-
-    fn divide(&self, plane: &Plane) -> (Option<ConvexHull>, Option<ConvexHull>) {
-        if self.points.len() == 1 {
-            if plane.distance(self.points[0]) >= 0 {
-                return (Some(self.clone()), None);
-            } else {
-                return (None, Some(self.clone()));
-            }
-        }
-
-        let front = self.cull(plane);
-        let back = self.cull(&plane.opposite());
-        (front, back)
-    }
-
-    fn cull(&self, plane: &Plane) -> Option<ConvexHull> {
-        if self.points.len() == 1 {
-            if plane.distance(self.points[0]) >= 0 {
-                return Some(self.clone());
-            } else {
-                return None;
-            }
-        }
-
-        let mut front = ConvexHull::new();
-
-        let lines = self.points[0..self.points.len()]
-            .iter()
-            .zip(self.points[1..].iter().chain(self.points[0..1].iter()));
-
-        for line in lines {
-            let d1 = plane.distance(*line.0);
-            let d2 = plane.distance(*line.1);
-
-            if d1 >= 0 {
-                front.points.push(*line.0);
-            }
-
-            if d1 * d2 < 0 {
-                let (x1, y1) = line.0;
-                let (x2, y2) = line.1;
-
-                let x = (x1 * d2 - x2 * d1) / (d2 - d1);
-                let y = (y1 * d2 - y2 * d1) / (d2 - d1);
-
-                front.points.push((x, y));
-            }
-        }
-
-        if front.points.is_empty() {
-            None
-        } else {
-            Some(front)
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -279,6 +163,7 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3 "#;
     }
 
     #[test]
+    #[ignore]
     fn test_part1() {
         let input = include_str!("../input/day_15.txt");
         let answer = solve_part1(input, 2000000);
@@ -288,60 +173,49 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3 "#;
     #[test]
     fn test_part2_sample() {
         let answer = solve_part2(SAMPLE_INPUT, 20);
-        assert_eq!(answer, 93);
+        assert_eq!(answer, 56000011);
     }
 
     #[test]
+    #[ignore]
     fn test_part2() {
         let input = include_str!("../input/day_15.txt");
-        let answer = solve_part2(input, 20);
+        let answer = solve_part2(input, 4000000);
         assert_eq!(answer, 28145);
     }
 
     #[test]
-    fn test_opposite_plane() {
-        let plane = Plane::new(Dir::Up, -10);
-        assert_eq!(plane.opposite(), Plane::new(Dir::Down, 11));
+    fn test_is_in_area() {
+        let (sx, sy, bx, by) = (10, 10, 20, 10);
+        assert_eq!(is_in_area(sx, sy, bx, by, 10, 10), true);
+        assert_eq!(is_in_area(sx, sy, bx, by, 20, 10), true);
+        assert_eq!(is_in_area(sx, sy, bx, by, 21, 10), false);
+        assert_eq!(is_in_area(sx, sy, bx, by, 0, 10), true);
+        assert_eq!(is_in_area(sx, sy, bx, by, -1, 10), false);
     }
 
     #[test]
-    fn test_divide_convex_hull() {
-        let mut cv = ConvexHull::new();
-        cv.points.push((0, 0));
-        cv.points.push((10, 0));
-        cv.points.push((10, 10));
-        cv.points.push((0, 10));
+    fn test_cover_points() {
+        let (sx, sy, bx, by) = (10, 10, 12, 10);
+        let cover_points = get_cover_points(sx, sy, bx, by);
 
-        let plane = Plane::new(Dir::Up, -5);
-        let (front, back) = cv.divide(&plane);
+        assert_eq!(cover_points.len(), 12);
         assert_eq!(
-            front.unwrap().points,
-            vec![(0, 0), (10, 0), (10, 5), (0, 5)]
-        );
-        assert_eq!(
-            back.unwrap().points,
-            vec![(10, 6), (10, 10), (0, 10), (0, 6)]
-        );
-
-        let plane = Plane::new(Dir::UpLeft, -1);
-        let (front, back) = cv.divide(&plane);
-        assert_eq!(front.unwrap().points, vec![(0, 0), (1, 0), (0, 1)]);
-        assert_eq!(
-            back.unwrap().points,
-            vec![(2, 0), (10, 0), (10, 10), (0, 10), (0, 2)]
-        );
-
-        let plane = Plane::new(Dir::UpLeft, 0);
-        let (front, back) = cv.divide(&plane);
-        assert_eq!(front.unwrap().points, vec![(0, 0)]);
-        assert_eq!(
-            back.unwrap().points,
-            vec![(1, 0), (10, 0), (10, 10), (0, 10), (0, 1)]
-        );
-
-        let plane = Plane::new(Dir::UpRight, 11);
-        let (front, back) = cv.divide(&plane);
-        assert_eq!(front, None);
-        assert_eq!(back, Some(cv));
+            cover_points,
+            [
+                (7, 10),
+                (8, 11),
+                (8, 9),
+                (9, 12),
+                (9, 8),
+                (10, 13),
+                (10, 7),
+                (11, 12),
+                (11, 8),
+                (12, 11),
+                (12, 9),
+                (13, 10)
+            ]
+        )
     }
 }
